8007559c8bf7abeb5f6cfa4dc67f62c85aeb3493
joneganagranado <joneganagranado@hotmail.com>
2021-09-17 11:26:27 +0000

    20-30 done

--- exercises/023-isOddLength/app.js
+++ exercises/023-isOddLength/app.js
@@ -1,1 +1,4 @@
 // Write your function here
+function isOddLength(val){
+    return (val.length%2!=0);
+}
--- exercises/028-cube/app.js
+++ exercises/028-cube/app.js
@@ -1,1 +1,4 @@
 // Write your function here
+function cube(num1){
+    return num1*num1*num1;
+}
